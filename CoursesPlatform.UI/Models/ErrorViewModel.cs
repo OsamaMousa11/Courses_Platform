84892604beb911cf6e94f91e51357226b6ca94b5
OsamaMousa11 <osamaomar1995@gmail.com>
2025-06-04 14:28:11 +0300

    Add project files.

---
+++ CoursesPlatform.UI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CoursesPlatform.UI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
