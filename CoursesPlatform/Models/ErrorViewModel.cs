9ef43a62b68068e66ebe0d073def046ebaaaec9c
Osama mousa <143362084+OsamaMousa11@users.noreply.github.com>
2025-06-08 17:09:51 +0300

    Delete CoursesPlatform directory

--- CoursesPlatform/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace CoursesPlatform.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
